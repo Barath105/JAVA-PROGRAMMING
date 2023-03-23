47ef24e77d734a4b00c249f40333495f82fc5236
Barath105 <113302780+Barath105@users.noreply.github.com>
2023-03-23 12:06:01 +0530

    Add files via upload

---
+++ ANALYTICAL QUESTION/pattern.java
@@ -0,0 +1,26 @@
+class pattern
+{
+public static void main(String args[])
+{
+int i,j;
+int n=1;
+for(i=1;i<=i;i++)
+{
+for(j=1;j<=i;j++)
+{
+System.out.print(i +"  ");
+}
+System.out.println();
+if(i==n)
+break;
+}
+for(i=n-1;i>=1;i--)
+{
+for(j=1;j<=i;j++)
+{
+System.out.print(i +"  ");
+}
+System.out.println();
+}
+}
+}
